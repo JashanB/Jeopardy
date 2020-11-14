954c87dde8031a2590a9bc74cf7929379fe85119
JashanB <jashan_brar50@hotmail.com>
2020-11-14 14:40:56 -0500

    setup folder structure

---
+++ src/components/board/Board.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import './Board.css'
+
+export default function Board(props) {
+  //use effect to set grid size based on # of tiles in row buttons
+  
+  return (
+    <li className="grid-container">
+      
+    </li>
+  )
+}
---
+++ src/components/question/Question.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import './Question.css'
+
+export default function Question(props) {
+  //use effect to set grid size based on # of tiles in row buttons
+  
+  return (
+    <li className="grid-container">
+      
+    </li>
+  )
+}
